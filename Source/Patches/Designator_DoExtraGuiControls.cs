4ed2a811d614f893358f947857af1e94df562e20
Shaun McFall <shaun@shaunmcfall.com>
2025-07-25 17:46:30 -0400

    Hide vanilla menu

---
+++ Source/Patches/Designator_DoExtraGuiControls.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace Merthsoft.DesignatorShapes.Patches;
+
+[HarmonyPatch(typeof(Designator), "DoExtraGuiControls")]
+internal static class Designator_DoExtraGuiControls
+{
+    public static bool Prefix()
+        => !DesignatorShapes.ShowControls;
+}
